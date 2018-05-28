9f38362c26eff7af111fe4c5250bed39fd3172e7
joshua morris <morrisons233@gmail.com>
2018-05-28 17:25:44 +0100

    Tweaked hostname method. Again

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
         <head>
                 <title>Blog.exe | <?php echo $title; ?></title>
-                <link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['HTTP_HOST']; ?>/css/main.css">
+                <link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/css/main.css">
         </head>
         <body>
         This is all a test.
